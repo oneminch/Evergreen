699e87a7067e9d409ed18620b0486cea707c7674
oneminch <27751688+oneminch@users.noreply.github.com>
2024-01-02 14:03:30 -0500

    feat: create a root page with a default loading indicator

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLoadingIndicator color="#fcc419" />
+  <AppLayout>
+    <NuxtPage />
+  </AppLayout>
+</template>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
