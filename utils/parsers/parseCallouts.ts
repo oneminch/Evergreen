4e6de5568bfdef4a602eea7466d788e232c10309
oneminch <27751688+oneminch@users.noreply.github.com>
2024-01-02 13:57:27 -0500

    feat: add parser utility for Callout syntax

---
+++ utils/parsers/parseCallouts.ts
@@ -0,0 +1,18 @@
+function calloutSyntaxReplacer(
+  match: string,
+  p1: string,
+  p2: string,
+  p3: string
+) {
+  const quoteContent = p3.replaceAll(/(^|\n)>\s/g, "$1");
+
+  return `${p1}\n::app-blockquote\n${quoteContent}\n::`;
+}
+
+const CALLOUT_REGEX = /(^|\n)>{1}\s(?:\[!(\w+)\])\n((>{1}\s.*(\n|$))*)/gi;
+
+const parseCallouts = (fileBody: string) => {
+  return fileBody.replaceAll(CALLOUT_REGEX, calloutSyntaxReplacer);
+};
+
+export default parseCallouts;
